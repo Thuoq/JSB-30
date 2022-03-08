122aca9f02f727243e07a02955208ff7cd9f68c2
JohnCowNut <thuonghamno3xz@gmail.com>
2022-03-08 18:25:59 +0700

    add buoi 6]

---
+++ Buoi 6/app.js
@@ -0,0 +1,88 @@
+
+
+// // while 
+// //
+
+// // lam sao de anh in ra tu 1 den 1000
+
+// // Cu phap 
+// // while(condition) {
+//     // code here 
+
+// //}
+// var i = 1; // i = 1
+// while(i <= 5) {  
+//     console.log(i)
+//     i++;
+// }
+// // bai 1: In tu 1 -> 1000
+
+// // Bai 2: In ra cac so chan tu 1 -> 20;
+// // Cach 1 
+
+// var i = 1;
+// while(i <= 20) { 
+//     if(i % 2 == 0) {
+//         console.log(i)
+//     }
+//     i++;
+// }
+// // Cach 2 
+// var i = 2 
+// while(i <=20) {
+//     console.log(i)
+//     i+=2;
+// }
+// // Bai 3: In ra cac so le tu 1 -> 20;
+
+// // Cach 1: 
+
+// var i = 1;
+
+// while (i <= 20) { 
+
+//     console.log(i)
+//     i+=2
+// }
+
+// // Cach 2
+// var i = 1;
+
+// while (i <= 20) { 
+//     if (i  % 2 == 1) { // dieu kien check so le 
+//         console.log(i) 
+//     }
+//     i++;
+// }
+// // Bai4: Tinh tong tu 
+// // 1 ->20 nghiax la: 1 +2 +  3 +4 +5 +6 + ...+20;
+
+// // 1+ 2+ 3+4+5+6+7+...+20;
+
+
+var i  = 1; 
+var sum = 0; // 0//0
+while(i <= 4 ) {  // (1<4) || (2<=4)
+   
+   // console.log(i) // 1 //2//3//4//5//6//7 
+    sum += i; // 1  // 3 + 4/ 7 // 10
+    i ++ ; //  3 / 4 //5
+}
+//console.log(sum)
+
+// Bai 6: Tinh tong cac co chan tu 1->20: 2 + 4 + 6+ 8 + 10 +... +20
+
+// In cho anh theo tu thu tu 
+// 10 -> 1
+
+var i = 3; 
+while( i>= 1 ) {  
+    console.log(i) // 3 // 2 // 1
+    i--; // 2 // 1 // 0
+}
+
+
+
+// BTVN
+
+// 
